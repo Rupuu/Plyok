42eeb689c9a912b4c7d7433f708f7d91f4cb3797
Rupuu <zamyarkov@gmail.com>
2025-01-25 21:12:34 +0200

    max num of enemies

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -9,7 +9,7 @@ const SPEED = 100
 @onready var animation_player = $AnimatedSprite2D
 
 func _ready():
-		# Set random velocity for the bubble
+	# Set random velocity for the bubble
 	velocity = Vector2(choose([-20, 20, -40, 40]), choose([-20, 20, -40, 40])).normalized() * SPEED
 	
 	# Rotate the bubble randomly
@@ -18,11 +18,11 @@ func _ready():
 	# Set random size for the bubble
 	var random_scale = choose([0.75, 1.0, 1.25])  # Choose a random scale factor
 	scale = Vector2(random_scale, random_scale)  # Apply uniform scaling
-	
+
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		#!!!!😱!!!! add change in srpite direction here.
+		# Change direction based on collision
 		velocity = velocity.bounce(collision.get_normal())
 		if ray_cast_right.is_colliding():
 			animation_player.flip_h = true
@@ -37,8 +37,11 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
 # Play the death animation and queue the node for removal
 func trigger_death():
 	animation_player.play("pop")
-	await animation_player.animation_finished # Wait for the animation to finish
-	queue_free() # Remove the bubble after the animation
+	# Wait for the animation to finish using a signal
+	animation_player.animation_finished.connect(_on_death_animation_finished)
+
+func _on_death_animation_finished():
+	queue_free()  # Remove the bubble after the animation
 
 func choose(array):
 	array.shuffle()
