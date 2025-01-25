d73e5f41772c846b9a2e540e5158925285edd80c
Rupuu <zamyarkov@gmail.com>
2025-01-25 16:55:38 +0200

    finished default bubble

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -4,19 +4,22 @@ class_name BubbleEnemy
 
 const SPEED = 100
 
-var dead: bool = false
-var got_hit: bool = false
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animation_player = $AnimatedSprite2D
 
 func _ready():
-	velocity = Vector2(100,-100).normalized() * SPEED
+	velocity = Vector2(choose([-20,20,-40,40]),choose([-20,20,-40,40])).normalized() * SPEED
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		#!!!!😱!!!! add change in srpite direction here.
 		velocity = velocity.bounce(collision.get_normal())
-
-@onready var animation_player = $AnimatedSprite2D
+		if ray_cast_right.is_colliding():
+			animation_player.flip_h = true
+		if ray_cast_left.is_colliding():
+			animation_player.flip_h = false
 
 # Handle input events (e.g., mouse clicks)
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
@@ -28,3 +31,7 @@ func trigger_death():
 	animation_player.play("pop")
 	await animation_player.animation_finished # Wait for the animation to finish
 	queue_free() # Remove the bubble after the animation
+
+func choose(array):
+	array.shuffle()
+	return array.front()
